478cf1bf8c8160547605231e3edb7a282e27f0a9
kentarou1005 <h.kentaro.1005@gmail.com>
2019-08-22 05:16:37 +0900

    update-js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,46 +22,60 @@ $(document).on('turbolinks:load', function() {
   }
 
   function buildMessageHTML(message) {
-      const same_part = 
-    '<div class="message" data-id=' + message.id + '>' +
-      '<div class="upper-message">' +
-        '<div class="upper-message__user-name">' +
-          message.user_name +
-        '</div>' +
-        '<div class="upper-message__date">' +
-          message.created_at +
-        '</div>' +
-      '</div>' ;
-
+      // var img = message.image ? `<img src= ${ message.image }>` : "";
     if (message.content && message.image.url) {
       //data-idが反映されるようにしている
-      var html = same_part
-      +
-        '<div class="lower-message">' +
-          '<p class="lower-message__content">' +
-            message.content +
-          '</p>' +
-          '<img src="' + message.image.url + '" class="lower-message__image" >' +
-        '</div>' +
-      '</div>'
+      var html = 
+        `<div class="message" data-id= ${message.id} > 
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name} 
+          </div>
+          <div class="upper-message__date">
+            ${message.created_at} 
+          </div>
+        </div> 
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content} 
+          </p>
+          <img src=${message.image.url} class="lower-message__image" >
+        </div>
+      </div>`
     } else if (message.content) {
       //同様に、data-idが反映されるようにしている
-      var html = same_part
-      +
-        '<div class="lower-message">' +
-          '<p class="lower-message__content">' +
-            message.content +
-          '</p>' +
-        '</div>' +
-      '</div>'
+      var html = 
+      `<div class="message" data-id= ${message.id} > 
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.user_name} 
+        </div>
+        <div class="upper-message__date">
+          ${message.created_at} 
+        </div>
+      </div> 
+      <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content} 
+          </p>
+        </div>
+      </div>`
     } else if (message.image.url) {
       //同様に、data-idが反映されるようにしている
-      var html = same_part
-      +
-        '<div class="lower-message">' +
-          '<img src="' + message.image.url + '" class="lower-message__image" >' +
-        '</div>' +
-      '</div>'
+      var html = 
+      `<div class="message" data-id= ${message.id} > 
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.user_name} 
+        </div>
+        <div class="upper-message__date">
+          ${message.created_at} 
+        </div>
+      </div> 
+      <div class="lower-message">
+          <img src=${message.image.url} class="lower-message__image" >
+        </div>
+      </div>`
     };
     return html;
   };
@@ -106,6 +120,7 @@ $(document).on('turbolinks:load', function() {
                 insertHTML = buildMessageHTML(message); 
                 $('.messages').append(insertHTML);
                     });
+                $("form")[0].reset();
                 $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, "fast");
             }
             else{
