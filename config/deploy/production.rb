c9ae32327a3ece91e4f5d9e1070ffc3162eebc38
kentarou1005 <h.kentaro.1005@gmail.com>
2019-08-25 14:26:45 +0900

    update.production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server 13.230.245.43, user: 'ec2-user', roles: %w{app db web}
+server '13.230.245.43', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
