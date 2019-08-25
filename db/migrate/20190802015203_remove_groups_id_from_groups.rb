8285ac20dfc59084442e3dca042b4a77ac711c6d
kentarou1005 <h.kentaro.1005@gmail.com>
2019-08-25 17:13:26 +0900

    update

--- db/migrate/20190802015203_remove_groups_id_from_groups.rb
+++ db/migrate/20190802015203_remove_groups_id_from_groups.rb
@@ -1,5 +1,5 @@
 class RemoveGroupsIdFromGroups < ActiveRecord::Migration[5.2]
   def change
-    remove_column :groups, :group_id, :string
+    remove_column :groups, :group_id, :integer
   end
 end
