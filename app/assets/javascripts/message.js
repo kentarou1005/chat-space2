387b8d9952c93ed56028a011a8a48eb221d4b8a8
kentarou1005 <h.kentaro.1005@gmail.com>
2020-01-06 16:03:44 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,29 +1,86 @@
-$(document).on('turbolinks:load', function() {
+$(function(){
 
-  function buildMessageHTML(message) {
+  // function buildMessageHTML(message) {
 
-    var content = message.content ? `${message.content}`  : "";
-    var image = message.image ? `<img src= ${message.image} class= "lower-message__image"> ` : "";
+  //   var content = message.content ? `${message.content}`  : "";
+  //   var image = message.image ? `<img src= ${message.image} class= "lower-message__image"> ` : "";
 
-    var html = 
-        `<div class="message" data-id= ${message.id} > 
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name} 
-          </div>
-          <div class="upper-message__date">
-            ${message.created_at} 
-          </div>
-        </div> 
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${content} 
-          </p>
-            ${image}  
-      </div>`
+  //   var html = 
+  //       `<div class="message" data-id= ${message.id} > 
+  //       <div class="upper-message">
+  //         <div class="upper-message__user-name">
+  //           ${message.user_name} 
+  //         </div>
+  //         <div class="upper-message__date">
+  //           ${message.created_at} 
+  //         </div>
+  //       </div> 
+  //       <div class="lower-message">
+  //         <p class="lower-message__content">
+  //           ${content} 
+  //         </p>
+  //           ${image}  
+  //     </div>`
       
+  //   return html;
+  // };
+
+//-----------------------------------------------------------
+  var buildMessageHTML = function(message) {
+    if (message.content && message.image) {
+      //data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    };
     return html;
   };
+//-----------------------------------------------------------
 
   $('.new_message').on('submit', function(e){
     e.preventDefault();
@@ -40,20 +97,25 @@ $(document).on('turbolinks:load', function() {
     })
 
     .done(function(message){
-      var html = buildMessageHTML(message);
-      $(".messages").append(html);
-      $('.form__message').val('');
-      $("form")[0].reset();
-      $(".messages").animate({scrollTop: $(".message").last().offset().top + $('.messages').scrollTop()}, 500, "swing");
-    })
-    .fail(function(){
-      alert('メッセージを入力してください');
-    });
+      console.log(message.content)
+      if (message.content !== undefined) { 
+        var html = buildMessageHTML(message);
+        $(".messages").append(html);
+        $("form")[0].reset();
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        }
+      else {
+        alert('メッセージを忘れていますよ');
+        }
+      })
     return false;
   });
+});
 
+$(function(){
   var reloadMessages = function(){
-      var last_message_id = $('.message:last').data('id')
+     last_message_id = $('.message:last').data('message-id');
+     console.log(last_message_id)
       $.ajax({
         url: "api/messages",
         type: "GET",
@@ -61,24 +123,30 @@ $(document).on('turbolinks:load', function() {
         data: {id: last_message_id}
       })
       .done(function(messages){
-        if (messages.length > 0){
-              var insertHTML = '';
-              messages.forEach(function (message) {
-                insertHTML = buildMessageHTML(message); 
-                $('.messages').append(insertHTML);
-                $("form")[0].reset();
-              });
-                $('.messages').animate({scrollTop: $('.messages')[0]}, "fast");
-              }
+        console.log(messages.content)
+        if (messages.length !== 0){
+          // var insertHTML = '';
+          //   messages.forEach(function (message) {
+          //   insertHTML += buildMessageHTML(message); 
+          //   $('.messages').append(insertHTML);
+          //   $("form")[0].reset();
+          //   $(".form__submit").prop("disabled", false);
+          //   console.log("success")
+          // });
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+          }
         else{
           }
-        })
+      })
+      
       .fail(function(){
-      });
+        })
       return false;
     };
-  setInterval(reloadMessages, 6000);
-});
-
-
-
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+      }
+    else {
+      console.log("新しい投稿はなし")
+      }
+})
